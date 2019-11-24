9552c655e0ffdef2b21d4d0bec31a634eee0d55b
Vitorhpx <vitorhugoperles@gmail.com>
2019-11-24 11:49:29 -0300

    Server side

--- src/server.py
+++ src/server.py
@@ -1,57 +1,61 @@
 import socket as socket
+import select as select
 import threading as threading
 
+HEADER_LENGHT = 10
 HOST = '127.0.0.1'
-PORT = 5001
-MAX_PENDING = 5
+PORT = 1234
+MAX_PENDING = 10
 MAX_LINE = 256
 MAX_SEND_LINE = 1024
 
 
-def thread_send_file(connection, address):
-    with connection:
-        print('Connected with:', address, "\n")
-        data = connection.recv(MAX_LINE)
-        if not data:
-            return
-
-        decodeData = data.decode()
-
-        [HTTPmethod, filePath, httpVersion, _host, hostName,
-         *_others] = decodeData.split()
-        filePath = filePath.split("/")[1]
-
-        if (HTTPmethod != "GET") or (httpVersion != "HTTP/1.1"):
-            print("Erro")
-            connection.sendall(str.encode("Error"))
-            connection.close()
-            return
-
-        address = socket.gethostbyname(hostName.split(':')[0])
-
-        try:
-            with open(filePath, mode='rb') as file:  # b is important -> binary
-                fileContent = file.read()
-
-            message = "HTTP/1.1 200 OK\nContent-Length: {fileLength}\nContent-Type: text\n\n".format(
-                fileLength=len(fileContent))
-            connection.send(str.encode(message), MAX_SEND_LINE)
-            connection.sendall(fileContent, MAX_SEND_LINE)
-            file.close()
-        except:
-            errorMessage = "HTTP/1.1 400 ERRO\n\n"
-            connection.send(str.encode(errorMessage), MAX_SEND_LINE)
-            file.close()
-        connection.close()
-        return
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGHT)
+        if not len(message_header):
+            return False
+        message_length = int(message_header.decode('utf-8').strip())
+        return {'header': message_header, "data": client_socket.recv(message_length)}
+    except:
+        return False
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(MAX_PENDING)
+    sockets_list = [s]
+    clients = {}
     while True:
-        connection, address = s.accept()
-        if connection:
-            threading.Thread(target=thread_send_file, args=(connection, address)).start()
+        read_sockets, _, exception_sockets = select.select(
+            sockets_list, [], sockets_list)
+        for notified_socket in read_sockets:
+            if notified_socket == s:
+                client_socket, client_address = s.accept()
+                user = receive_message(client_socket)
+                if user is False:
+                    continue
+                sockets_list.append(client_socket)
+                clients[client_socket] = user
+                print('Accepted new connection from {}:{}, username: {}'.format(
+                    *client_address, user['data'].decode('utf-8')))
+            else:
+                message = receive_message(notified_socket)
+                if message is False:
+                    print('Closed connection from: {}'.format(
+                        clients[notified_socket]['data'].decode('utf-8')))
+                    sockets_list.remove(notified_socket)
+                    del clients[notified_socket]
+                    continue
+                user = clients[notified_socket]
+                print(
+                    f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+                for client_socket in clients:
+                    if client_socket != notified_socket:
+                        client_socket.send(
+                            user['header'] + user['data'] + message['header'] + message['data'])
 
-
+        for notified_socket in exception_sockets:
+            sockets_list.remove(notified_socket)
+            del clients[notified_socket]
